fe32523a5038c1f992a1206f20d0d5605c7daf23
Bogdan21o312 <lutskiv2010@gmail.com>
2022-11-11 18:41:14 +0200

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Card from "./components/Card";
 import lorem from "./assets/db.json"
 import Loader from "./components/Loader";
+import ButtonNeon from "./components/ButtonNeon";
 
 const App = () => {
     return (
         <div>
-            <Loader size={'medium'}/>
+            <ButtonNeon/>
         </div>
     );
 };
