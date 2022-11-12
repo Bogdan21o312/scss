25b37eb414783d39290047cbd32373c1efb16df3
Bogdan21o312 <lutskiv2010@gmail.com>
2022-11-12 15:26:16 +0200

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import Card from "./components/Card";
-import lorem from "./assets/db.json"
-import Loader from "./components/Loader";
 import ButtonNeon from "./components/ButtonNeon";
 
 const App = () => {
